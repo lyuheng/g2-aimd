eaff7984826cd08f30af961b072bb7bdb9b14daa
akhlaqueak <akhlaque.ak@gmail.com>
2024-02-22 01:47:03 +0300

    revision

--- kmeans/preprocess.cpp
+++ kmeans/preprocess.cpp
@@ -3,11 +3,14 @@
 
 int main(int argc, char *argv[])
 {
-
+        Timer timer;
+    timer.StartTimer();
     CommandLine cmd(argc, argv);
     std::string filename = cmd.GetOptionValue("-f", "./data/com-dblp.ungraph.txt");
     Graph graph(filename);
     
     graph.Preprocess();
     graph.writeBinFile(filename);
+    timer.EndTimer();
+    timer.PrintElapsedMicroSeconds("preprocessing completed: ");
 }
